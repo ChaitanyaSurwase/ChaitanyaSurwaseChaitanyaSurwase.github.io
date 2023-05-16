9aad8cef064a963bec8e781180949af7d45ad094
ChaitanyaSurwase <surwasechaitanya001@gmail.com>
2023-05-16 06:43:49 +0530

    done with ai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ShowProjectComponent } from './show-project/show-project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'new-project', component: NewProjectComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'show-project', component: ShowProjectComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule } from  '@angular/common/http';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ShowProjectComponent } from './show-project/show-project.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';  
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ShowProjectComponent,
+    NewProjectComponent, NavbarComponent,DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule ,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
